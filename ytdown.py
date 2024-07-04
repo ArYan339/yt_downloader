73d862f6bd04a14a51efba4f9bc41454021ec5e9
ArYan Si <117717585+ArYan339@users.noreply.github.com>
2024-07-04 16:50:21 +0545

    Update ytdown.py

--- ytdown.py
+++ ytdown.py
@@ -3,9 +3,6 @@ import yt_dlp
 import os
 from pathlib import Path
 
-st.set_page_config(page_title="Video Downloader", page_icon="🎥", layout="wide")
-st.server.max_request_size = 1000  # Increase the request size limit (in MB)
-st.server.max_message_size = 1000  # Increase the message size limit (in MB)
 
 def get_available_formats(url):
     ydl_opts = {'quiet': True, 'no_warnings': True}
